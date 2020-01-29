81c742f82ef5060bb681f35069ee440abd09167a
MaxIvanov44 <Lvanovmaksim200033@gmail.com>
2020-01-29 20:15:47 +0300

    mvcroute

---
+++ RoutingMVC/RoutingMVC/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingMVC.Controllers
+{
+    [Route("Store")]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [Route("Main")]     // сопоставляется с Home/Main, либо с Store/Main
+        [Route("Index")] // сопоставляется с Home/Index, либо с Store/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RoutingMVC/RoutingMVC/Startup.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace RoutingMVC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            
+        }
+        public IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(mvcOtions =>
+            {
+                mvcOtions.EnableEndpointRouting = false;
+            });
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
